a31a7de17ede3d1e7eba2c4461dec3a756a2da99
Risto A. Paju <teknohog@iki.fi>
2011-09-15 21:43:10 +0300

    known good blockdata for debugging

--- projects/Verilog_Xilinx_Port/miner.py
+++ projects/Verilog_Xilinx_Port/miner.py
@@ -61,8 +61,12 @@ class Writer(Thread):
 
         # Keep something sensible available while waiting for the
         # first getwork
-        self.block = "0" * 256
-        self.midstate = "0" * 64
+        #self.block = "0" * 256
+        #self.midstate = "0" * 64
+
+        # This will produce nonce 063c5e01 -> debug by using a bogus URL
+        self.block = "0000000120c8222d0497a7ab44a1a2c7bf39de941c9970b1dc7cdc400000079700000000e88aabe1f353238c668d8a4df9318e614c10c474f8cdf8bc5f6397b946c33d7c4e7242c31a098ea500000000000000800000000000000000000000000000000000000000000000000000000000000000000000000000000080020000"
+        self.midstate = "33c5bf5751ec7f7e056443b5aee3800331432c83f404d9de38b94ecbf907b92d"
 
         self.daemon = True
 
